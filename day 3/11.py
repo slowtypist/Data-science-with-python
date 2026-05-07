6228f30b5a03169c4acd8735932d9302ad4a12c8
Soumili Biswas <soumilibiswas.github@gmail.com>
2026-05-07 16:20:32 +0530

    Add files via upload

---
+++ day 3/11.py
@@ -0,0 +1,8 @@
+#There is no 'on' in both dragon and python
+a = "python"
+b = "dragon"
+if "on" in a and "on" in b :
+    print(False)
+else:
+    print(True)
+
---
+++ day 3/5.py
@@ -0,0 +1,5 @@
+a = int(input("enter the side a: "))
+b = int(input("enter the side b: "))
+c = int(input("enter the side c: "))
+perimeter = a+b+c
+print(f"the perimeter is {perimeter}")
