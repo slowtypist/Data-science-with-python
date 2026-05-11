da81345a79ad9bd3536a4fa91d18a910131fc302
Soumili Biswas <soumilibiswas.github@gmail.com>
2026-05-12 02:28:55 +0530

    Add mymodule.py with greeting functions

---
+++ mdules/mymodule.py
@@ -0,0 +1,11 @@
+#create code
+def say_hello(name):
+    return print(f"hello , {name}")
+
+#we will use this file in the module.py file
+
+
+def say_bye(name):
+    return print(f"bye , {name}")
+
+person_1 = {'name':'keshav', 'age':16}
