f3102d14bdcb8be279be98c44abf9ec39ede415e
Soumili Biswas <soumilibiswas.github@gmail.com>
2026-05-11 21:24:58 +0530

    Create average.py

---
+++ day9/average.py
@@ -0,0 +1,10 @@
+def cal_average(a,b,c):
+    average = (a+b+c)/2
+    print(average)
+    return average
+
+cal_average(8,5,6)
+x = cal_average(8,5,6) 
+print(x)
+#with return value 
+#output 9.5,9.5,9.5
