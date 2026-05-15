51389e45ced3ca4a2938e90956952ffb2f489178
Soumili Biswas <soumilibiswas.github@gmail.com>
2026-05-15 20:18:24 +0530

    Add person dictionary and print middle skill

---
+++ revision 15/6.py
@@ -0,0 +1,18 @@
+person={
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_married': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+    }
+
+print("skills" in person)
+if "skills" in person:
+    skills = person["skills"]
+    middle = skills[len(skills)//2]
+    print(middle)
