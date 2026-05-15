45872832d637eacc8a59539bcf6ab05ea2906a8d
Soumili Biswas <soumilibiswas.github@gmail.com>
2026-05-15 20:18:49 +0530

    Add person dictionary with skills printing

---
+++ revision 15/7.py
@@ -0,0 +1,16 @@
+person = {
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_marred': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'
+    }
+}
+for key in person:
+    if key == 'skills':
+        for skill in person['skills']:
+            print(skill)
