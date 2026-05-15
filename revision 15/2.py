37a967a1d94f446f2c7d64fdfd019badfac5f5fe
Soumili Biswas <soumilibiswas.github@gmail.com>
2026-05-15 19:20:45 +0530

    Create 2.py

---
+++ revision 15/2.py
@@ -0,0 +1,8 @@
+tpl1 = ("mili", "beatrix", "kiddo")
+tpl2 = ("uma", "tarantino", "budd")
+final = tpl1+tpl2
+print(final)
+print(len(final))
+sibling = list(final)
+sibling.extend(["mala", "dada"])
+print(sibling)
